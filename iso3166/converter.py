7bac5974d84119b0d69d1fbf659fc03366d5f04a
Omar Gluhic <omargluhic@SYM0561.local>
2022-10-05 11:29:57 +0200

    feat: add string distance calculator and placeholder code for converter functin

--- iso3166/converter.py
+++ iso3166/converter.py
@@ -9,27 +9,55 @@ DATA = pd.read_csv(DATA_PATH, dtype=str)
 
 def country_name_conversion(df: pd.DataFrame,
                             *,
-                            input_format: str,
-                            output_format: str,
+                            input_format: str = "name",
                             fuzzy_threshold: int = 0,
                             sample_size: int = 5) -> None:
 
     """TODO Make validators for input variables"""
 
-    target_column = _auto_found_column(df, input_format, sample_size)
+    available_formats = {"name", "official", "alpha-2", "alpha-3"}
+
+    if input_format not in available_formats:
+        raise ValueError()
+
+    # Detect column
+    target_column = _auto_find_column(df, input_format, sample_size)
 
     pass
 
 
-def _auto_found_column(df: pd.DataFrame, input_format: str, sample_size: int) -> str:
+def _auto_find_column(df: pd.DataFrame, input_format: str, sample_size: int) -> str:
+
+    target_column = DATA[input_format].str.lower()
+    target_column = target_column.str.replace(" ", "").values
 
     for col in df.columns:
         for sample in df[col].sample(sample_size):
-            if sample in DATA[input_format]:
+            if str(sample).lower().strip() in target_column:
                 return col
 
     """TODO raise an error and log it, double check time complexity"""
 
 
+def _format_country_name(val: str, input_format: str, fuzzy_threshold: int) -> str:
+
+    country = str(val).replace(" ", "").lower()
+
+def _find_country(country: str, input_format: str, fuzzy_threshold: int) -> str:
+    pass
+
 if __name__ == "__main__":
-    print(DATA_PATH)
+    test_df = pd.DataFrame(
+        {
+            "messy_country": [
+                "Canada",
+                "foo canada bar",
+                "cnada",
+                "northern ireland",
+                " ireland ",
+                "Denmark"]
+        })
+
+    x = _auto_find_column(test_df, "name", 5)
+    print(x)
+    print("Done")
--- iso3166/utils.py
+++ iso3166/utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 
 def read_data(path: str) -> pd.DataFrame:
@@ -16,7 +17,6 @@ def read_data(path: str) -> pd.DataFrame:
 
 
 def _load_file(path: str) -> pd.DataFrame:
-
     """ TODO define outcome for no file type, place into try/except block with logger
     TODO define better catch them all function, add more file type loaders"""
 
@@ -26,7 +26,7 @@ def _load_file(path: str) -> pd.DataFrame:
     _, file_type = os.path.split(path)
 
     if file_type is None:
-        pass # raise error and log it
+        pass  # raise error and log it
 
     elif file_type in file_functions:
         return file_functions[file_type](path)
@@ -35,5 +35,35 @@ def _load_file(path: str) -> pd.DataFrame:
     return pd.read_excel(path)
 
 
-def column_finder(df: pd.DataFrame, conversion_type: str) -> pd.Series:
-    pass
+def calculate_levenshtein_ratio(base_str: str, target_str) -> float:
+    # Create matrix
+    rows = len(base_str) + 1
+    cols = len(target_str) + 1
+
+    zero_matrix = np.zeros((rows, cols), dtype=int)
+
+    # Populate matrix with characters of both strings
+    for i in range(1, rows):
+        for k in range(1, cols):
+            zero_matrix[i][0] = i
+            zero_matrix[0][k] = k
+
+    # Find cost of substitution
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if base_str[row - 1] == target_str[col - 1]:
+                cost = 0
+            else:
+                cost = 2
+
+            # Cost of deletion, insertion and substitution
+            zero_matrix[row][col] = min(zero_matrix[row - 1][col] + 1,
+                                        zero_matrix[row][col - 1] + 1,
+                                        zero_matrix[row - 1][col - 1] + cost)
+
+    return ((len(base_str)+len(target_str)) - zero_matrix[row][col]) / (len(base_str)+len(target_str))
+
+
+if __name__ == "__main__":
+    x = calculate_levenshtein_ratio("canada", "cnada")
+    print(x)
