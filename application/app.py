5cd19d8117d3e076837d188b59bbd56673d8ba86
Omar Gluhic <omargluhic@SYM0561.local>
2022-11-30 14:57:41 +0100

    fix: fixed size and workflow of the s3 upload

--- application/app.py
+++ application/app.py
@@ -8,30 +8,27 @@ from chalicelib.iso3166.utils import load_to_s3
 from chalicelib.core.config import settings
 
 app = Chalice(app_name=settings.PROJECT_NAME)
-ACCESS_KEY = 'test'
-SECRET_KEY = 'test'
-SESSION_TOKEN = 'test'
 
 s3_client = boto3.client(
     "s3",
     endpoint_url="http://host.docker.internal:4566",
     use_ssl=False,
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-    aws_session_token=SESSION_TOKEN)
+    aws_access_key_id=settings.ACCESS_KEY,
+    aws_secret_access_key=settings.SECRET_KEY,
+    aws_session_token=settings.SESSION_TOKEN)
 
 
-@app.on_s3_event(bucket=settings.INPUT_BUCKET,
-                 events=["s3:ObjectCreated:*"])
+@app.on_s3_event(bucket=settings.INPUT_BUCKET, events=["s3:ObjectCreated:*"])
 def handle_object_creation(event):
-
-    response = s3_client.get_object(Bucket=settings.INPUT_BUCKET, Key=event.key)
+    # try:
+    response = s3_client.get_object(Bucket=settings.INPUT_BUCKET,
+                                    Key=event.key)
 
     with io.BytesIO(response['Body'].read()) as data:
         df1, df2 = lambda_name_standardization_factory(data=data,
                                                        file_name=event.key)
-
         # Load data to output bucket
+
         load_to_s3(s3_client=s3_client,
                    destination=settings.OUTPUT_BUCKET,
                    name="silver/{}".format(event.key),
@@ -42,4 +39,4 @@ def handle_object_creation(event):
         load_to_s3(s3_client=s3_client,
                    destination=settings.OUTPUT_BUCKET,
                    name=f"error_report/{event.key}-{current_time}",
-                   dataframe=df1)
+                   dataframe=df2)
--- application/chalicelib/core/config.py
+++ application/chalicelib/core/config.py
@@ -9,6 +9,10 @@ class ApplicationSettings:
     PROJECT_NAME: str = "ISO-3166-Standardizer"
     INPUT_BUCKET: str = getenv("INPUT_BUCKET_NAME")
     OUTPUT_BUCKET: str = getenv("OUTPUT_BUCKET_NAME")
+    ACCESS_KEY: str = getenv("ACCESS_KEY")
+    SECRET_KEY: str = getenv("SECRET_KEY")
+    SESSION_TOKEN: str = getenv("SESSION_TOKEN")
 
 
 settings = ApplicationSettings()
+
